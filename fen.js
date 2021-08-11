66e4fd57696b3008cf75c779450d6df156afc534
Joseph Novakovich <josephnovakovich@gmail.com>
2021-08-11 09:48:54 -0400

    fen from lichess analysis boards

--- fen.js
+++ fen.js
@@ -1,18 +1,40 @@
 // this "script" runs when the extension icon is clicked
-var FEN = 'FEN: ';
+var FEN_CT = 'FEN: ';
 
-function extract_fen () {
-    var txt = document.body.innerText;
-    var a = txt.search(FEN);
-    if (a>0) {
-	var b = txt.substring(a).search('\n');
-	var fen = txt.substring(a+FEN.length,a+b);
+function yank_fen (fen) {
 	navigator.clipboard.writeText(fen).then(
 	    () => {},
 	    () => alert('failed to copy fen')
 	);
+}
+
+function chesstempo_fen () {
+    var txt = document.body.innerText;
+    var a = txt.search(FEN_CT);
+    if (a>0) {
+	var b = txt.substring(a).search('\n');
+	var fen = txt.substring(a+FEN_CT.length,a+b);
+	yank_fen (fen);
    } else {
 	alert('no fen to copy');
+    }
+}
+
+function lichess_fen () {
+    var fenpgn = document.getElementsByClassName('fen-pgn');
+    var fen = fenpgn[0].children[0].children[1].value;
+    console.log(fen);
+    yank_fen (fen);
+}
+
+function extract_fen () {
+    var where = window.location.host;
+    if (where === 'chesstempo.com') {
+	chesstempo_fen();
+    } else if (where === 'lichess.org') {
+	lichess_fen ();
+    } else {
+	alert('idk how to get a fen from ',where);
     }
 }
 
