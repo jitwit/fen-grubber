29be4a8eda2416df75f9d5cf74f83b4c0f30974e
jitwit <josephnovakovich@gmail.com>
2025-05-26 12:06:56 -0400

    minorly

--- fen.js
+++ fen.js
@@ -26,7 +26,7 @@ function chesstempo_fen () {
 
 function crossword_clue () {
   var clue = document.getElementsByClassName('selected')[1].innerText;
-  clue = clue.substring(clue.indexOf('\n') + 1);
+  clue = clue.substring(clue.search('\n') + 1);
   open('https://google.com/search?q=site:tryhardguides.com ' + clue);
 }
 
