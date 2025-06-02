e66645e253a557fa571a1ccaad7333cb73c485ec
jitwit <josephnovakovich@gmail.com>
2025-06-02 15:20:03 -0400

    minorly

--- crossword-link.js
+++ crossword-link.js
@@ -1,6 +1,6 @@
 function write_link () {
-  var td = ":TimDumper:";
-  var link = `${td} @Virgins ${td} ${window.location.href} ${td}`;
+  var td = ":TimDumper:"; var url = window.location.href;
+  var link = `${td} ${url} ${td} @Virgins ${td}`;
   navigator.clipboard.writeText(link).then(
     () => {},
     () => alert('failed to write link')
