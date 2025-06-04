e1d9832f46a7f8a0762f8327b47d3db005e8b502
jitwit <josephnovakovich@gmail.com>
2025-06-04 17:19:41 -0400

    minorly

--- chess-freenalysis.js
+++ chess-freenalysis.js
@@ -1,15 +1,11 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 async function import_to_lichess (pgn) {
   try {
     var response = await fetch("https://lichess.org/api/import", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Accept": "application/json"
-      },
+      headers: { "Content-Type": "application/x-www-form-urlencoded",
+		 "Accept": "application/json" },
       body: `pgn=${encodeURIComponent(pgn)}`
     });
     if (!response.ok) { throw new Error(`monacoS: ${response.status}`); }
@@ -27,6 +23,7 @@ async function analyse_freely () {
   document.getElementById("tab-pgn").click();
   await sleep(500);
   document.getElementsByClassName("share-menu-tab-pgn-copy")[0].click();
+//  #share-modal > div > div.cc-modal-body.cc-modal-sm > header > button
   var pgn = await navigator.clipboard.readText();
   import_to_lichess (pgn);
 }
