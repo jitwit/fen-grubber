53a7966f389a6eeff58cd939a2cdacf973b130f1
jitwit <josephnovakovich@gmail.com>
2025-07-02 10:17:28 -0400

    suppress "google ai" (udm=14)

--- crossword-clue.js
+++ crossword-clue.js
@@ -2,9 +2,9 @@
 
 // search might return last example of? had small issue with: Law & Order actor ___ de la Garza
 function crossword_clue () {
-  var clue = document.getElementsByClassName('selected')[1].innerText;
-  clue = clue.substring(clue.search('\n') + 1);
-  open('https://google.com/search?q=site:tryhardguides.com ' + clue);
+  var clue = document.getElementsByClassName("selected")[1].innerText;
+  clue = clue.substring(clue.search("\n") + 1);
+  open("https://google.com/search?udm=14&q=site:tryhardguides.com " + clue);
 }
 
 if (window.location.host === "downforacross.com") { crossword_clue (); }
