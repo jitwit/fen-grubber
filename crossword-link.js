6479f6fd68418915d131b81f22b4cfeb11e91e70
Joseph Novakovich <josephnovakovich@gmail.com>
2026-03-09 15:33:41 -0400

    minorly

--- crossword-link.js
+++ crossword-link.js
@@ -9,4 +9,4 @@ function write_link () {
   );
 };
 
-if (window.location.host === "crosswithfriends.com") { write_link (); }
+if (window.location.host === "www.crosswithfriends.com") { write_link (); }
